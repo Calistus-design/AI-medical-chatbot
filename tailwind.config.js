acfeeb65217446bfd3852ca41e74bb8a92f1db41
Calistus-design <calistusmwonga@gmail.com>
2025-10-30 10:09:39 +0300

    tailwind css updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 // File: tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true, 
+  
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
