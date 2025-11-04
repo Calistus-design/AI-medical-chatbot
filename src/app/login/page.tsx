fabdb5d18f1a228f864c9e6461f6f9298be2a866
Calistus-design <calistusmwonga@gmail.com>
2025-11-04 13:58:48 +0300

    login update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,6 +8,23 @@ export default function LoginPage() {
       <Typography component="h1" variant="h5" align="center" gutterBottom>
         Log In
       </Typography>
+      {/* --- ADD THIS TEMPORARY DEBUGGING BLOCK --- */}
+      <Typography 
+        variant="caption" 
+        align="center" 
+        sx={{ 
+          display: 'block', 
+          p: 2, 
+          border: '1px dashed red', 
+          mb: 2, 
+          wordBreak: 'break-all' 
+        }}
+      >
+        DEBUG: NEXT_PUBLIC_SUPABASE_URL is: 
+        <br />
+        <strong>{process.env.NEXT_PUBLIC_SUPABASE_URL || "VALUE IS NOT SET"}</strong>
+      </Typography>
+      {/* --- END OF DEBUGGING BLOCK --- */}
       <AuthForm mode="login" /> {/* Pass the mode prop */}
     </Container>
   );
